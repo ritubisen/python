661712e6080b8592ebc08203e23fb9a2cb08b59c
RITU BISEN <ritub1050@gmail.com>
2025-11-12 11:44:51 +0530

    oops

---
+++ filehandling.py/oppss.py
@@ -0,0 +1,48 @@
+#*******************************************(((((((((((((((((((((((((((((((((((((    OOPS    )))))))))))))))))))))))))))))))))))))*************************************
+
+  # 1.   class ==========  design of an object
+                 
+  # 
+  # 2.   object             
+
+
+# #class==>>>>>>>>> 
+# class classname :
+#     "doc string "
+#     variables (properties)
+           
+#            instance variable
+#            class/static variable
+#            local variable
+    
+
+#     methods (action/behavior)
+      
+#          constructor
+#    -->  instance method 
+#     --> class  metod
+#     --> static method
+
+
+# class mee address nikalna hai
+
+# class student:
+#       pass
+
+# print(id(student))
+# obj1=student
+# print (id(obj1))
+# obj2=student
+# print(id(obj2))
+
+# obekct ke sath 
+
+class student:
+      school = 'SHSS'
+      def showdeatil():
+            print("wlvomto schil")
+
+print(dir(student))
+print(student.__doc__)
+
+
